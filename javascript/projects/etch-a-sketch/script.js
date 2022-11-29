67b31aa1f0e242e3349fe79f78070a1a9b81c19b
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-11-28 17:01:36 -0800

    new bg color, added 'blank'/'filled' classes, fixed grid templates

--- javascript/projects/etch-a-sketch/script.js
+++ javascript/projects/etch-a-sketch/script.js
@@ -1,32 +1,45 @@
 // Etch-a-Sketch
 console.log("by: gesty 🏄");
+
 /* TODO
-  * prompt should specify 1 side, function should multiply for square
-  * new grid should overwrite old grid
   * grid square limit (100x100)
-  * '.container' border instead of blue bg?
+    - input validation on prompt
   * '.container' fixed width/height
 */
+
 const container = document.querySelector('.container');
 
 // creating div grid
 function createDivGrid(gridNum) {
   for (let i = 0; i < gridNum; i++) {
     let div = document.createElement('div');
-    div.setAttribute('style', 'background: white;');
+    div.classList.add('blank');
     container.append(div);
 
     // hover effect
     div.addEventListener('mouseover', () => {
-      div.setAttribute('style', 'background: black;');
+      //div.classList.remove('blank');
+      div.classList.add('filled');
     })
   }
 }
-createDivGrid(16);
+createDivGrid(16); // 16 == 4x4 grid
 
 // Grid Squares button
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-  let newNum = prompt("How many grid squares do you want?");
-  createDivGrid(newNum);
+  let newNum = prompt("How many grid squares do you want?\n" + 
+    "1-10 (for one side)");
+
+  // remove old grid
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+
+  // create new grid
+  createDivGrid(newNum * newNum);
+
+  // new container dims / grid template
+  let newTemplate = '100px '.repeat(newNum);
+  container.setAttribute('style', `grid-template-columns: ${newTemplate}`)
 })
