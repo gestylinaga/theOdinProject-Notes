ca1fd11fdd23cf41c76eb86142164e2859713ee0
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-12-03 03:06:31 -0800

    added example books, started book display

--- javascript/projects/library/js/app.js
+++ javascript/projects/library/js/app.js
@@ -1,6 +1,19 @@
-// Library - Project by: gesty 🏄
+// Library 
+console.log('by: gesty 🏄');
+
+/* TODO
+  * add a "new book" button w/ form allowing users to input book details
+    * fix `submit` button issue w/ `event.preventDefault();`
+  * add button on each book display to remove the book from library
+    * data-attributes
+  * add button on each book display to change read/not read status
+  * function to toggle status on prototype
+
+  * grid display for books?
+*/
 
 let library = [];
+const container = document.querySelector('.container');
 
 // Book Object constructor
 function Book(title, author, pages, read) {
@@ -10,39 +23,53 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-// Example Book
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+// Example Books
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 library.push(theHobbit);
-//console.log(theHobbit)
-
+const fightClub = new Book('Fight Club', 'Chuck Palahniuk', 224, true);
+library.push(fightClub);
+const neuromancer = new Book('Neuromancer', 'William Gibson', 271, false);
+library.push(neuromancer);
 
 // Function to add books to library
 function addBookToLibrary() {
 }
 
-
 // Function to display books
 function displayBooks() {
+  for (Book of library) {
+    let bookDisplay = document.createElement('div');
+    bookDisplay.classList.add('card');
+
+    let header = document.createElement('h2');
+    header.textContent = Book.title;
+    bookDisplay.appendChild(header);
+
+    let author = document.createElement('p');
+    author.textContent = `by ${Book.author}`
+    bookDisplay.appendChild(author);
+
+    let pageNum = document.createElement('p');
+    pageNum.textContent = `${Book.pages} pages`
+    bookDisplay.appendChild(pageNum);
+
+    let readStatus = document.createElement('h3');
+    if (Book.read) {
+      readStatus.setAttribute('style', 'color: green;')
+      readStatus.textContent = 'Already Read';
+    } else {
+      readStatus.setAttribute('style', 'color: red;')
+      readStatus.textContent = 'Not Read Yet';
+    };
+    bookDisplay.appendChild(readStatus);
+
+    /*
+    let readButton = document.createElement('button');
+    readButton.textContent = 'Read?';
+    bookDisplay.appendChild(readButton);
+    */
+
+    container.appendChild(bookDisplay);
+  }
 }
-
-
-/* TODO
-  * store book objects in a simple array
-  * add function to script (not constructor) that takes input to store a new book
-
-  * function that loops through array to display each book - table or cards
-  * add some sample books to visualize better
-
-  * add a "new book" button w/ form allowing users to input book details
-    * title
-    * author
-    * number of pages
-    * read/not read
-  * fix `submit` button issue w/ `event.preventDefault();`
-
-  * add button on each book display to remove the book from library
-    * data-attributes
-
-  * add button on each book display to change read/not read status
-  * function to toggle status on prototype
-*/
+displayBooks();
