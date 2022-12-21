b3d1079ca6798f9545e45ec597125fc3cde75778
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-12-20 20:07:55 -0800

    started gameBoard & player factory function

--- javascript/projects/ticTacToe/script.js
+++ javascript/projects/ticTacToe/script.js
@@ -1,2 +1,34 @@
 // Tic-Tac-Toe 
 console.log("by: gesty 🏄");
+
+const playerCreator = (name, symbol) => {
+  name = name;
+  symbol = symbol;
+  return { name, symbol };
+};
+
+const gameBoard = {
+  createGrid: function() {
+    const container = document.querySelector('.container');
+    let grid = [];
+    for (let i = 0; i < 9; i++) {
+      const div = document.createElement('div');
+      div.addEventListener('click', () => {
+      div.style.background = 'gray';
+    });
+    grid.push(div);
+    container.appendChild(div);
+    };
+    console.log(grid);
+  },
+};
+
+const mainGame = {
+  start: gameBoard.createGrid(),
+  currentPlayer: function() {
+    // ...
+  },
+};
+
+const player1 = playerCreator('Player 1', 'X');
+const player2 = playerCreator('Player 2', 'O');
